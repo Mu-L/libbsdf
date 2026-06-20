7cccbcddd2905775060d502610e3fdcb4a98c4a5
Kimura Ryo <ryki@mac.com>
2026-06-20 18:21:56 +0900

    Fixed deprecated sprintf warning

--- src/Common/Version.cpp
+++ src/Common/Version.cpp
@@ -1,5 +1,5 @@
 // =================================================================== //
-// Copyright (C) 2015 Kimura Ryo                                       //
+// Copyright (C) 2015-2026 Kimura Ryo                                  //
 //                                                                     //
 // This Source Code Form is subject to the terms of the Mozilla Public //
 // License, v. 2.0. If a copy of the MPL was not distributed with this //
@@ -10,10 +10,6 @@
 
 #include <stdio.h>
 
-#ifdef _MSC_VER
-#pragma warning(disable : 4996)
-#endif
-
 using namespace lb;
 
 const char* lb::getVersion()
@@ -22,7 +18,7 @@ const char* lb::getVersion()
     static char version[256];
 
     if (!initialized) {
-        sprintf(version, "%d.%d.%d", LIBBSDF_MAJOR_VERSION, LIBBSDF_MINOR_VERSION, LIBBSDF_PATCH_VERSION);
+        snprintf(version, sizeof(version), "%d.%d.%d", LIBBSDF_MAJOR_VERSION, LIBBSDF_MINOR_VERSION, LIBBSDF_PATCH_VERSION);
         initialized = true;
     }
 
